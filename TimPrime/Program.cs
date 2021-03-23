14ff901e610240e04299a247ba2781d1630cab4c
minhduyet129 <minhduyet129@gmail.com>
2021-03-23 23:34:32 +0700

    Upload bt

---
+++ TimPrime/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+namespace TimPrime
+{
+    class Program
+    {
+        static Task<bool> FindPrimeNumberAsync(int n)
+        {
+            return Task.Run<bool>(() =>
+             {
+                 int dem = 0;
+                 for (int i = 1; i <= n; i++)
+                 {
+                     if (n % i == 0) dem++;
+                     if (dem > 2) break;
+                 }
+                 return dem == 2 ? true : false;
+             });
+        }
+        static async Task Main(string[] args)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            for (int i = 1; i < 100; i++)
+            {
+                if (await FindPrimeNumberAsync(i)) Console.WriteLine(i);
+
+            }
+            Console.WriteLine(stopWatch.Elapsed);
+            stopWatch.Stop();
+        }
+    }
+}
